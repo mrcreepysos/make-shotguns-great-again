1595f2ffa3e1b93429bfba035a4e744198132b1a
nikita <59087849+mrcreepysos@users.noreply.github.com>
2023-12-03 02:14:35 +0300

    fix u240.3 crashes

---
+++ lua/firetweakdata.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(FireTweakData, "_init_dot_entries_fire", "msga__init_dot_entries_fire", function(self)
+	self.dot_entries.fire.ammo_dragons_breath.dot_trigger_max_distance = 3000 -- restore old db range
+end)
